aa04bfb4543d6c911ead26f455ddde9f74d7f59b
bsunsten <47073544+bsunsten@users.noreply.github.com>
2019-11-02 13:16:17 -0500

    Got file reading working

--- PA2.c
+++ PA2.c
@@ -1,24 +1,46 @@
 #include<stdlib.h> 
 #include<stdio.h>
 #include <stdint.h> 
+#include <inttypes.h>
+#include <endian.h>
 
 
 int main () {
 
-  FILE *fp = fopen("tmp.machine", "rw");
-  fseek(fp, 0L, SEEK_END);
-  int sz = ftell(fp);
-  rewind(fp);
+  FILE *fp = fopen("tmp.txt.machine", "r");
 
-  
+  if(fp == NULL)
+    printf("Error in file opening\n");
+
+  fseek(fp, 0L, SEEK_END);
+
+  int sz = ftell(fp);
+  fseek(fp, 0L, SEEK_SET);
+
   printf("Byte size of buffer is %d\n",sz);
 
   uint32_t* buffer;
-  
-  buffer = (uint32_t*) malloc((sz/4) * sizeof(uint32_t));
 
-  fread(buffer, sz, 8, fp);
-  printf("%s\n", buffer);
+  buffer = (uint32_t*) malloc(sz);
+
+
+
+  
+
+  fread(buffer, 1, sz, fp);
+
+  for(int i = 0; i < (sz/4); i++){
+
+    buffer[i] = be32toh(buffer[i]);
+
+  }
+
+
+  printf("The contents of the buffer: %x \n", (unsigned long)buffer[0]);
+
+  fclose(fp);
+  return 0;
+  
 
   
 
