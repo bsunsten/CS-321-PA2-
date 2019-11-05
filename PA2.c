44caad9febf72a65e188ddbce0d4cd51b18028b6
bsunsten <47073544+bsunsten@users.noreply.github.com>
2019-11-04 20:23:01 -0600

    Begun adding opcode functions

--- PA2.c
+++ PA2.c
@@ -7,21 +7,22 @@
 
 
 uint32_t decimalToBinary(uint32_t n);
-
+uint32_t checkInstruction(uint32_t input);
 
 
 int main (int argc, char *argv[]) {
 
   //TODO: Implement get value from parsed cmd argument
-  int memsize = 1024
+  int memsize = 1024;
 
 
   //allocate space for registers
   uint32_t registers[32];
 
   //create an enum for use with registers to make things more readable
-  enum regNames = {X0, X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, X11, X12, X13, X14, X15, X16, X17, X18, X19, X20, X21, X22, X23, X24, X25, X26, X27, X28, X29, X30, X31};
+  enum regNames {X0, X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, X11, X12, X13, X14, X15, X16, X17, X18, X19, X20, X21, X22, X23, X24, X25, X26, X27, X28, X29, X30, X31};
 
+  //create a simulated main memory with the given number of addresses
   uint32_t mainmem[memsize]; 
 
   uint32_t* buffer;
@@ -53,9 +54,17 @@ int main (int argc, char *argv[]) {
 
   }
 
+
+  for (int i = 0; i < sz/4; i++){
+
+
+    checkInstruction(buffer[i]);
+
+  }
+
   
   
-  printf("The contents of the buffer: %032lu \n", buffer[0]);
+  printf("The contents of the buffer: %032x \n", buffer[0]);
 
   fclose(fp);
   return 0;
@@ -63,3 +72,42 @@ int main (int argc, char *argv[]) {
 
 }
 
+//INSERT YOUR CODE HERE
+//function reads an instruction line by line and determines 
+//the instruction to run based off of the opcode
+uint32_t checkInstruction(uint32_t input){
+
+
+uint32_t opcode = (input << (0)) >> (32-11);
+
+ printf("Instruction Opcode is %032lu\n", opcode); 
+
+
+ //EORI
+ if(opcode >= 840 && opcode <= 841) {
+
+   EORI(input);
+
+ }
+
+ //HALT
+ if(opcode == 2047){
+
+   HALT(input);
+
+ }
+
+ //LDUR
+if(opcode == 1986){
+
+   LDUR(input);
+
+ }
+
+//LDURB
+
+
+
+
+ return opcode;
+}
